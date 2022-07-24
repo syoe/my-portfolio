6b26048607c45629e0313ba8bfaeb32f8533aba8
syoe <shoe657sunflower@yahoo.co.jp>
2022-07-24 18:41:58 +0900

    アイスクリーム更新

--- main.js
+++ main.js
@@ -1,6 +1,11 @@
 const button = document.getElementById("button");
 const displayItems = document.getElementById("display-items");
+const displayThanks = document.getElementById("display-thanks");
 
 button.onclick = function () {
   displayItems.textContent = displayItems.textContent + "🍨";
+  displayThanks.textContent = "ありがと〜💕💕💕";
+  if (textContent % Number === !0) {
+    displayThanks.textContent + "素数だ〜〜〜！！！ありがと！！！💕💕💕";
+  }
 };
